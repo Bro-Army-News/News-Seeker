79eeb49cebf51e36e4bfcdc21d34ddb2e9a3d94a
Pasindu0113 <66360636+Pasindu0113@users.noreply.github.com>
2021-02-25 09:59:21 +0530

    Development of the sign up page frontend part

--- NewsSeekerUI/App.js
+++ NewsSeekerUI/App.js
@@ -1,7 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import WelcomeScreen from './app/screens/WelcomeScreen';
+import SignUp from './app/screens/SignUp';
 
 export default function App() {
-  return <WelcomeScreen/>
+  return <SignUp/>
 }
---
+++ NewsSeekerUI/app/screens/SignUp.js
@@ -0,0 +1,130 @@
+import React from 'react';
+import { Image, ScrollView, View, StyleSheet,Text, TextInput, TouchableOpacity, SafeAreaView } from 'react-native';
+
+function Login(props) {
+    return (
+        <View style={styles.body}>
+            <View style={styles.header}>
+                <Image style={styles.headerLogo} source={require('../assets/Logo.jpg')}/>
+                <Text style={styles.headerText}>News Seeker</Text>
+            </View>
+            <SafeAreaView style={styles.container}>
+                <Text style={styles.containerHeader}>SignUp</Text>
+                <ScrollView style={styles.ScrollView}>
+                
+                    <TextInput 
+                    placeholder="UserName"
+                    style={styles.input}/>
+                    <TextInput 
+                    placeholder="Email"
+                    style={styles.input}/>
+                    <TextInput 
+                    placeholder="Date of Birth"
+                    style={styles.input}/>
+                    <TextInput 
+                    placeholder="Country"
+                    style={styles.input}/>
+                    <TextInput 
+                    placeholder="Password"
+                    style={styles.input}/>
+                    <TextInput 
+                    placeholder="Re-Enter Password"
+                    style={styles.input}/>
+                    <TouchableOpacity style={styles.submit}>
+                        <Text style={styles.submitText}>SIGN UP HERE</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={[styles.submit, styles.submitColor1]}>
+                        <Text style={styles.submitText}>SIGN UP WITH EMAIL</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={[styles.submit, styles.submitColor2]}>
+                        <Text style={styles.submitText}>SIGN UP WITH FACEBOOK</Text>
+                    </TouchableOpacity>
+                </ScrollView>    
+            </SafeAreaView>
+        </View>
+    );
+}
+
+
+
+const styles = StyleSheet.create({
+    body:{
+        backgroundColor:'#5C5C5C',
+        flex:1,
+        justifyContent:'flex-end',
+    },
+    header:{
+        flexDirection:'row',
+        top:-40,
+        left:45,
+    },
+    headerLogo:{
+        width:60,
+        height:60,
+        borderRadius:10,
+    },
+    headerText:{
+        fontStyle:'italic',
+        fontWeight: 'bold',
+        color:'linen',
+        fontSize:40,
+        top:2,
+        left:20,
+    },
+    container:{
+        width:'100%',
+        height:'75%',
+        backgroundColor:'linen',
+        borderTopLeftRadius:20,
+        borderTopRightRadius:20,
+    },
+    containerHeader:{
+        textAlign:'center',
+        marginTop:20,
+        fontWeight: 'bold',
+        fontSize:28,
+        color:'green',
+    },
+
+    ScrollView:{
+        top: 0, 
+        left: 35, 
+        bottom: 0, 
+        right: 0},
+        
+    input:{
+        borderColor:'black',
+        borderWidth:2,
+        padding:5,
+        width:350,
+        marginBottom:40,
+    },
+    submit:{
+        marginHorizontal:30,
+        paddingTop:10,
+        paddingBottom:10,
+        borderRadius:30,
+        marginRight:100,
+        backgroundColor:'mediumseagreen',
+    },
+    submitColor1:{
+        marginTop:35,
+        marginLeft:35,
+        backgroundColor:'tomato',
+    },
+    submitColor2:{
+        marginTop:25,
+        backgroundColor:'cornflowerblue',
+    },
+    submitText:{
+        color:'#fff',
+        textAlign:'center',
+        fontWeight:'bold',
+    },
+    links:{
+        marginTop:15,
+        textAlign:'center',
+    }
+})
+
+export default Login;
