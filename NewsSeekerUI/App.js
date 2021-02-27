3fe5067ab9684ff54fe40bb489657c9008a6952a
hamza2019701 <hamza.2019701@iit.ac.lk>
2021-02-27 12:39:36 +0530

    Search screen 1st commit
    Search bar + background color

--- NewsSeekerUI/App.js
+++ NewsSeekerUI/App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 import WelcomeScreen from './app/screens/WelcomeScreen';
 import SignUp from './app/screens/SignUp';
 import Login from './app/screens/Login';
+import Search from './app/screens/Search';
 
 export default function App() {
-  return <SignUp/>
+  return <Search/>
 }
---
+++ NewsSeekerUI/app/screens/Search.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import {View, StyleSheet, StatusBar, TextInput} from 'react-native';
+
+const Search = () => {
+    return(
+        <View style={styles.maincontainer}>
+            <View style={styles.container}>
+                <TextInput style={styles.searchInput} placeholder="Search here..." />
+            </View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    maincontainer: {
+        marginTop: StatusBar.currentHeight,
+        paddingHorizontal: 15,
+        backgroundColor: '#5C5C5C',
+        flex: 1,
+    },
+    container: {
+        width: '100%',
+        height: 50,
+        backgroundColor: 'white',
+        borderRadius: 8,
+        justifyContent: 'center',
+    },
+    searchInput: {
+        width: '100%',
+        height: '100%',
+        paddingLeft: 8,
+        fontSize: 16,
+    },
+})
+
+export default Search;
