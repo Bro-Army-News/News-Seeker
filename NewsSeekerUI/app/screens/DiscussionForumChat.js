4fc5bf20a7df6a2393a2a1fc119f730a45137d95
thevindur <thevindu.2019595@iit.ac.lk>
2021-04-16 22:59:49 +0530

    Finished the Discussion Home Page and the Chat Screen.(Frontend)

---
+++ NewsSeekerUI/app/screens/DiscussionForumChat.js
@@ -0,0 +1,135 @@
+import React, {useState, useEffect, useCallback} from 'react';
+import {View, Text, StyleSheet, Image} from 'react-native';
+import {Bubble, GiftedChat, Send} from 'react-native-gifted-chat';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+
+const DiscussionForumChat = () => {
+    const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    setMessages([
+      {
+        _id: 1,
+        text: 'Hello NewsSeeker',
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: 'React Native',
+          avatar: 'https://placeimg.com/140/140/any',
+        },
+      },
+      {
+        _id: 2,
+        text: 'News Title One Appears Here',
+        createdAt: new Date(),
+        user: {
+          _id: 1,
+          name: 'React Native',
+          avatar: 'https://placeimg.com/140/140/any',
+        },
+      },
+    ]);
+  }, []);
+
+  const onSend = useCallback((messages = []) => {
+    setMessages((previousMessages) =>
+      GiftedChat.append(previousMessages, messages),
+    );
+  }, []);
+
+  const renderSend = (props) => {
+    return (
+      <Send {...props}>
+        <View>
+          <MaterialCommunityIcons
+            name="send-circle"
+            style={{marginBottom: 5, marginRight: 5}}
+            size={32}
+            color="#2e64e5"
+          />
+        </View>
+      </Send>
+    );
+  };
+
+  const renderBubble = (props) => {
+    return (
+      <Bubble
+        {...props}
+        wrapperStyle={{
+          right: {
+            backgroundColor: '#2e64e5',
+          },
+        }}
+        textStyle={{
+          right: {
+            color: '#fff',
+          },
+        }}
+      />
+    );
+  };
+
+  const scrollToBottomComponent = () => {
+    return(
+      <FontAwesome name='angle-double-down' size={22} color='#333' />
+    );
+  }
+
+//   const headerFormat = () =>{
+//       return(
+//         <View style={styles.header}>
+//             <Image style={styles.headerLogo} source={require('../assets/Logo.jpg')}/>
+//             <Text style={styles.headerText}>News Seeker</Text>
+//         </View>
+//       );
+//   }
+
+  return (
+    <GiftedChat
+        messages={messages}
+        onSend={(messages) => onSend(messages)}
+        user={{
+            _id: 1,
+        }}
+        // headerFormat={headerFormat}
+        renderBubble={renderBubble}
+        alwaysShowSend
+        renderSend={renderSend}
+        scrollToBottom
+        scrollToBottomComponent={scrollToBottomComponent}
+    />
+    
+  );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    header:{
+        flexDirection:'row',
+        backgroundColor:'#e7e6e1',
+        padding:10,
+    },
+    headerLogo:{
+        width:30,
+        height:30,
+        borderRadius:10,
+        left:120,
+
+    },
+    headerText:{
+        fontStyle:'italic',
+        fontWeight: 'bold',
+        color:'black',
+        fontSize:20,
+        top:2,
+        left:140,
+    },
+})
+
+export default DiscussionForumChat;
